ee4a30f621b9d13bc0cd69d8bf6517079b7f774d
Markus Koechl <markus.koechl@autodesk.com>
2024-04-17 23:34:36 +0200

    updated solution for 2025; fixed issue for iLogicVaultInventorServer sample rules: override default iLogicVault library types by adding 'non-ADSK to the library reference

--- iLogic-VaultInvServer Library/Properties/AssemblyInfo.cs
+++ iLogic-VaultInvServer Library/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Autodesk")]
 [assembly: AssemblyProduct("iLogic-VaultInventorServer Library")]
-[assembly: AssemblyCopyright("Copyright © 2023 Autodesk, Markus Koechl")]
+[assembly: AssemblyCopyright("Copyright © 2024 Autodesk, Markus Koechl")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2024.2.0.0")]
-[assembly: AssemblyFileVersion("2024.2.0.0")]
+[assembly: AssemblyVersion("2025.0.0.0")]
+[assembly: AssemblyFileVersion("2025.0.0.0")]
